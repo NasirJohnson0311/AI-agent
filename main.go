f1b7959695be6332c709f96e2032c70192ec944e
Nasir Johnson <nasir11buckets@icloud.com>
2026-09-19 18:24:31 -0500

    Updated main.go with Get functions

--- main.go
+++ main.go
@@ -14,15 +14,17 @@ type Vehicle struct {
 
 func main() {
 
-	var object1 Vehicle // Object of vehicle type
+	var object1 Vehicle 	// Create object of vehicle type
 
+	// Create arrays for each vehicle characteristic 
 	var vehicleID = [5]string{"V-100", "V-101", "V-102", "V-103", "V-104"}
 	var vehicleBattery = [5]float64{99.0, 58.2, 76.9, 28.23, 80.0}
 	var vehicleSpeed = [5]float64{42, 76, 95, 26, 33}
 	var vehicleTemp = [5]float64{100, 70, 80, 90, 59}
 	var vehicleLocation = [5]string{"Austin", "Lexington", "Alabama", "New York", "Oklahoma"}
 	var vehicleStatus = [5]string{"ACTIVE", "CHARGING", "OFFLINE", "ACTIVE", "ACTIVE"}
-
+	
+	// Assign characteristics to vehicle object
 	for i := 0; i < 5; i++ {
 		object1.ID = vehicleID[i]
 		object1.Battery = vehicleBattery[i]
@@ -31,15 +33,36 @@ func main() {
 		object1.Location = vehicleLocation[i]
 		object1.Status = vehicleStatus[i]
 
-		fmt.Println("Vehicle", i)
-		fmt.Println("Vehicle: " + object1.ID)
-		fmt.Println("Battery:", object1.Battery)
-		fmt.Println("Speed:", object1.Speed)
-		fmt.Println("Temperature:", object1.Temperature)
-		fmt.Println("Location: " + object1.Location)
-		fmt.Println("Status: " + object1.Status)
-
+		// Print characteristics of vehicle object
+		GetVehicleID(object1)
+		GetVehicleBattery(object1)
+		GetVehicleSpeed(object1)
+		GetVehicleTemperature(object1)
+		GetVehicleLocation(object1)
+		GetVehicleStatus(object1)
 		fmt.Println()
+
 	}
 
+}		// End of main
+
+func GetVehicleID(object1 Vehicle) {
+	fmt.Println("Vehicle ID: ",object1.ID)
 }
+func GetVehicleBattery(object1 Vehicle){
+	fmt.Println("Battery: ",object1.Battery)
+}
+func GetVehicleSpeed(object1 Vehicle){
+	fmt.Println("Speed: ",object1.Speed)
+}
+func GetVehicleTemperature(object1 Vehicle){
+	fmt.Println("Temperature: ",object1.Temperature)
+}
+func GetVehicleLocation(object1 Vehicle){
+	fmt.Println("Location: ",object1.Location)
+}
+func GetVehicleStatus(object1 Vehicle){
+	fmt.Println("Status: ",object1.Status)
+}
+
+
